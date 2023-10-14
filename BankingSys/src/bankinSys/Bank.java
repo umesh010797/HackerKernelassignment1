858646eb5689cf9e3b49fa0be505a3d8cf2fea94
umesh <umesh010797@gmail.com>
2023-10-15 00:40:53 +0530

    nun

--- BankingSys/src/bankinSys/Bank.java
+++ BankingSys/src/bankinSys/Bank.java
@@ -7,34 +7,38 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Bank {
-	
-	
-	public static void main(String agr[]) {
-		
-		
-		List<Customer> customers =new ArrayList<>();
-	    List<Account> accounts= new ArrayList<>();
-		Customer customer1=new Customer(101,"umesh",1012);
-		Customer customer2=new Customer(102,"rohit",1013);
-		Customer customer3=new Customer(103,"akshay",1014);
-		 customers.add(customer1);
-		 customers.add(customer2);
-		 customers.add(customer3);
-		
-		
-		Account acc1=new Account(201, customer1, 10000.0);
-		Account acc2=new Account(202, customer2, 150000.0);
-		Account acc3=new Account(203, customer3, 200000.0);
-		accounts.add(acc1);
-		accounts.add(acc2);
-		accounts.add(acc3);
-		
-		
-		System.out.println(accounts);
-		
-		Scanner scanner = new Scanner(System.in);
-		
-		while (true) {
+
+
+    public static void main(String agr[]) {
+
+
+        List<Customer> customers =new ArrayList<>();
+        List<Account> accounts= new ArrayList<>();
+        Customer customer1=new Customer(101,"umesh",1012);
+        Customer customer2=new Customer(102,"rohit",1013);
+        Customer customer3=new Customer(103,"akshay",1014);
+        customers.add(customer1);
+        customers.add(customer2);
+        customers.add(customer3);
+
+
+        Account acc1=new Account(201, customer1, 10000.0);
+        Account acc2=new Account(202, customer2, 150000.0);
+        Account acc3=new Account(203, customer3, 200000.0);
+        accounts.add(acc1);
+        accounts.add(acc2);
+        accounts.add(acc3);
+
+
+        //System.out.println(accounts);
+        for (Account account: accounts
+             ) {System.out.println(account);
+
+        }
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
             System.out.println("\nChoose an option:");
             System.out.println("1. Deposit");
             System.out.println("2. Withdraw");
@@ -57,12 +61,11 @@ public class Bank {
                 } else {
                     System.out.println("Failed to deposit.");
                 }
-                
 
 
-		}
+            }
             if (choice == 2) {
-            	System.out.print("Enter account number: ");
+                System.out.print("Enter account number: ");
                 int accountNumber = scanner.nextInt();
                 System.out.print("Enter amount to withdraw: ");
                 double amount = scanner.nextDouble();
@@ -74,35 +77,35 @@ public class Bank {
                 } else {
                     System.out.println("Failed to deposit.");
                 }
-	}			
+            }
             if(choice==3) {
-            	System.out.print("Enter Customer ID: ");
-            	int customerId = scanner.nextInt();
-            	List<Customer> cust=customers.stream().filter(x->x.getId()==customerId).collect(Collectors.toList());
-            	
-            	if (cust != null) {
+                System.out.print("Enter Customer ID: ");
+                int customerId = scanner.nextInt();
+                List<Customer> cust=customers.stream().filter(x->x.getId()==customerId).collect(Collectors.toList());
+
+                if (cust != null) {
                     System.out.println("Customer Details");
                     System.out.println(cust);
                 } else {
                     System.out.println("Customer Not find");
                 }
-            	
-            	
+
+
             }
             
             if(choice==4) {
-            	System.out.print("Enter account number: ");
-            	int accountNo = scanner.nextInt();
-            	List<Account> acc=accounts.stream().filter(x->x.getAccNo()==accountNo).collect(Collectors.toList());
-            	Account account= acc.get(0);
-            	if (account != null) {
+                System.out.print("Enter account number: ");
+                int accountNo = scanner.nextInt();
+                List<Account> acc=accounts.stream().filter(x->x.getAccNo()==accountNo).collect(Collectors.toList());
+                Account account= acc.get(0);
+                if (account != null) {
                     System.out.println("Account Details");
                     System.out.println(account);
                 } else {
                     System.out.println("Account Not find");
                 }
-            	
-            	
+
+
             }
             else if (choice == 5) {
                 System.out.println("Exiting...");
